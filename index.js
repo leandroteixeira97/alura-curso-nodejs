6fb6dd97734ea6bbd01c99944ba8ee4ac71417ac
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2023-02-28 22:50:12 -0300

    Codes from chapter 2 and 3

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+import fs from 'fs';
 import chalk from "chalk";
 
-console.log(chalk.blue('Olá mundo!'));
+function handleError(error) {
+    throw new Error(chalk.red(error.code, 'There is no file in such directory'));
+}
+
+async function getFile(path) {
+    const encoding = 'utf-8';
+    try {
+        const response = await fs.promises.readFile(path, encoding);
+        extractLinks(response);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+function extractLinks(text) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const captures = [...text.matchAll(regex)];
+    const results = captures.map((capture) => {
+        return { [capture[1]]: capture[2] };
+    });
+    console.log(results)
+}
+getFile('./files/text.md');
